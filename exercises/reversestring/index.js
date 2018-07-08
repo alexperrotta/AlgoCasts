aaa077a6e7b7f13980b75d501be72daf66d50509
Alex Perrotta <alexandra.perrotta@gmail.com>
2018-07-08 16:24:02 -0400

    reversed string solutions

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,56 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+
+/* solution 1 - reverse()
+Steps:
+Turn str into an array
+Call reverse method on the array
+Join the array back into a string
+Return the result
+*/
+
+// function reverse(str) {
+// 	return str.split('').reverse().join('');
+// }
+
+
+
+/* soluton 2 - for loop
+Steps:
+Create an empty string
+For each character in the string - take the character and add it to the start of the new string
+Return the variable
+*/
+
+
+// function reverse(str) {
+// 	let reversed = '';
+
+// 	// ES6 syntax for the for loop
+// 	for (let character of str) {
+// 		reversed = character + reversed;
+// 	}
+
+// 	return reversed;
+// }
+
+
+
+
+
+
+/* Solution 3 - reduce()
+Recommended in interview
+*/
+
+function reverse(str) {
+	return str.split('').reduce((reversed, char) => char + reversed, '');
+}
+
+
+
 
 module.exports = reverse;
+
+
