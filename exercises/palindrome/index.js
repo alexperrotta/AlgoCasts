02b826b542b58604d3f197e7f70d6de57f077468
Alex Perrotta <alexandra.perrotta@gmail.com>
2018-07-09 20:38:41 -0400

    completed palindrome

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,26 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+
+// solution 1 - reversed()
+function palindrome(str) {
+	const reversed = str.split('').reverse().join('');
+
+	return str === reversed;
+}
+
+
+// solution 2 - every() - not an ideal solution b/c you are comparing the first to the last element and the last to the first
+// this is doing twice the amount of work that we need to do
+function palindrome(str) {
+	// i is index
+	return str.split('').every((char, i) => {
+		return char === str[str.length - i - 1];
+	});
+}
+
+
+
+
 
 module.exports = palindrome;
