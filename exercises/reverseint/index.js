35380f06b0d2c33287d152b368ea571cd6941b59
Alex Perrotta <alexandra.perrotta@gmail.com>
2018-07-09 20:54:19 -0400

    completed reverseint

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,16 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+// Math.sign()
+// toString()
+// parseInt()
+
+function reverseInt(n) {
+	const reversed =  n.toString().split('').reverse().join('');
+
+	// Math.sign() will return a positive number or a negative number
+	return parseInt(reversed) * Math.sign(n);
+
+}
 
 module.exports = reverseInt;
