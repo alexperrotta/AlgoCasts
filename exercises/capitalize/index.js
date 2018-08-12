17cc98ea7f588d978ee7573ae7d5f974a1e9fbc9
Alex Perrotta <alexandra.perrotta@gmail.com>
2018-08-12 16:25:04 -0400

    capitalization

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,40 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+
+// Steps
+// Make an empty array "words"
+// split the input string by spaces to get an array
+// for each word in the array:
+	// uppercase the first letter of the word
+	// join the first letter with the rest of the string
+	// push result into 'words' array
+// Join 'words' into a string and return it
+
+function capitalize(str) {
+	const words = [];
+
+	for (let word of str.split(' ')) {
+		words.push(word[0].toUpperCase() + word.slice(1));
+	}
+
+	return words.join(' ');
+}
+
+// solution 2
+
+function capitalize(str) {
+	let result = str[0].toUpperCase();
+
+	for (let i = 1; i < str.length; i++) {
+		if (str[i - 1] === ' ') {
+			result += str[i].toUpperCase();
+		} else {
+			result += str[i];
+		}
+	}
+
+	return result;
+}
 
 module.exports = capitalize;
