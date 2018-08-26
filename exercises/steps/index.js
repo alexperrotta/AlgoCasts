0240422f443c193727c5ba5d79cf31383b78a5fb
Alex Perrotta <alexandra.perrotta@gmail.com>
2018-08-25 21:58:42 -0400

    completed steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,62 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+
+/*
+From 0 to n (iterate through rows)
+	Create an emptry string 'stair'
+	From 0 to n (iterate through columns)
+		If the current column is equal to or less than the current row
+			Add a '#' to 'stair'
+		Else
+			Add a space to 'stair'
+		Console log 'stair'
+*/
+
+// Solution 1 - use this in interviews
+
+function steps(n) {
+	for (let row = 0; row < n; row++) {
+		let stair = '';
+
+		for (let column = 0; column < n; column++) {
+			if (column <= row) {
+				stair += '#';
+			} else {
+				stair =+ ' '; // space
+			}
+		}
+		console.log(stair);
+	}
+}
+
+
+// Solution 2 - using recursion
+/*
+base case - if (row === n) the we have hit the end of our problem
+if the 'stair' string has a length === n then we are at the end of the row
+if the length of the stair string is less than or equal to the row number, we add
+'#', otherwise add a space
+*/
+
+function steps(n, row = 0, stair = '') {
+	// base case
+	if (n === row) {
+		return;
+	}
+
+	// case when we've hit the end of a row
+	if (n === stair.length) {
+		console.log(stair);
+		return steps(n, row + 1);
+	}
+
+	if (stair.length <= row) {
+		stair += '#';
+	} else {
+		stair += ' ';
+	}
+	steps(n, row, stair);
+}
 
 module.exports = steps;
