2b94021c770395719e15fdbcd03e6879e8e8f2f6
Alex Perrotta <alexandra.perrotta@gmail.com>
2018-08-26 21:01:37 -0400

    completed matrix

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -1,6 +1,10 @@
 // --- Directions
 // Write a function that accepts an integer N
 // and returns a NxN spiral matrix.
+
+// returns a 2D array, an array within an array
+// start at 1 and go counterclockwise, spirals inward
+
 // --- Examples
 //   matrix(2)
 //     [[1, 2],
@@ -15,6 +19,65 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+/* definitely think this out on the whiteboard before coding
+Create an empty array called 'results'
+Create a counter variable starting at 1
+As long as (start column <= end column) AND (start row <= end row)
+	Loop from start column to end column
+		At results[start_row][i] assign counter variable
+		Increment counter
+	Increment start row
+	Loop from start row to end row
+		At results[i][end_column] assign counter variable
+		Increment counter
+	Decrement end column
+	...repeat for other two sides
+*/
+
+function matrix(n) {
+	const results = [];
+
+	for (let i = 0; i < n; i++) {
+		results.push([]);
+	}
+
+	let counter = 1;
+	let startColumn = 0;
+	let endColumn = n - 1;
+	let startRow = 0;
+	let endRow = n - 1;
+	while (startColumn <= endColumn && startRow <= endRow) {
+		// top row
+		for (let i = startColumn; i <= endColumn; i++) {
+			results[startRow][i] = counter;
+			counter++;
+		}
+		// starts the 2nd row
+		startRow++;
+
+		// right column
+		for (let i = startRow; i <= endRow; i++) {
+			results[i][endColumn] = counter;
+			counter++;
+		}
+		endColumn--;
+
+		//bottom row - decrementing
+		for (let i = endColumn; i >= startColumn; i--) {
+			results[endRow][i] = counter;
+			counter++;
+		}
+		endRow--;
+
+		// start column
+		for (let i = endRow; i >= startRow; i--) {
+			results[i][startColumn] = counter;
+			counter++;
+		}
+		startColumn++;
+	}
+
+	return results;
+}
 
 module.exports = matrix;
